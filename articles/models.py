55d7334e9a4159a4b112204cb5298e2990c83d6a
yunji3 <leeyunji1984@naver.com>
2022-06-03 20:51:32 +0900

    과제

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model): #상속
+    title = models.CharField(max_length=10) #용량을 줄이기 위해선 짧은건 CharField로
+    content = models.TextField() # 많은 양을 쓸때 , 자주쓰면 용량차지 많이함.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
